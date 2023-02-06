38c4fb794436b3dc75149fcd4851819d1a9a369a
Davidleiva504 <105253456+Davidleiva504@users.noreply.github.com>
2023-02-05 18:51:48 -0600

    a

--- Trabajo4.js
+++ Trabajo4.js
@@ -4,44 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/Asuntos/', (req, res)=>{
+app.get('/api/persona/', (req, res)=>{
+
+
+    /*Lo que yo programe acá sera la logica 
+    que se ejecutara cuando se consuma el WS con el metodo get 
+    a la ruta  http://localohost:3000/api/persona/  */
 
 
     let con = mysql.createConnection({
         host: "127.0.0.1",
         user: "root",
-        password: "",
-        database: "Tarea 4"
+        password: "passa1234b",
+        database: "deswebq12023"
     });
-    let sql = "select * from N_expediente, cliente, F_inicio, F_final";
 
-    con.connect(function(err){
-
-        if (err){
-            res.send(err);
-        }else{
-            con.query(sql, function(err, result){
-
-                if (err){
-                    res.send(err);
-                }else{
-                    res.send(result);
-                }
-            });
-        }
-    } );
-
-});
-app.get('/api/Cliente/', (req, res)=>{
-
-
-    let con = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "root",
-        password: "",
-        database: "Tarea 4"
-    });
-    let sql = "select * from DNI, Nombre, Direccion";
+    let sql = "select * from tbl_persona";
 
     con.connect(function(err){
 
@@ -61,24 +39,25 @@ app.get('/api/Cliente/', (req, res)=>{
 
 });
 
-app.get('/api/Estado/', (req, res)=>{
+app.get('/api/persona/:id', (req,res)=>{
 
 
     let con = mysql.createConnection({
         host: "127.0.0.1",
         user: "root",
-        password: "",
-        database: "Tarea 4"
+        password: "passa1234b",
+        database: "deswebq12023"
     });
-    let sql = "select * from Tramite, Archivo";
+
+    let sql = "select * from tbl_persona where id_persona = ?";
+    let parametros = [req.params.id];
 
     con.connect(function(err){
 
         if (err){
             res.send(err);
         }else{
-            con.query(sql, function(err, result){
-
+            con.query(sql, parametros, function(err, result){
                 if (err){
                     res.send(err);
                 }else{
@@ -86,28 +65,34 @@ app.get('/api/Estado/', (req, res)=>{
                 }
             });
         }
-    } );
+    });
 
 });
 
-app.get('/api/Procuradores/', (req, res)=>{
-
+app.post('/api/persona/', (req, res)=>{
 
     let con = mysql.createConnection({
         host: "127.0.0.1",
         user: "root",
-        password: "",
-        database: "Tarea 4"
+        password: "passa1234b",
+        database: "deswebq12023"
     });
-    let sql = "select * from DNIP, Nombre, Direccion";
+
+    let sql = "insert into tbl_persona " +
+            " (nombre_persona, apellido_persona, fecha_nacimiento) " +
+            " values (?, ?, ?)";
+    
+    let parametros = [  req.body.nombre_persona, 
+                        req.body.apellido_persona, 
+                        req.body.fecha_nacimiento
+                    ];
 
     con.connect(function(err){
 
         if (err){
             res.send(err);
         }else{
-            con.query(sql, function(err, result){
-
+            con.query(sql, parametros, function(err, result){
                 if (err){
                     res.send(err);
                 }else{
@@ -115,6 +100,75 @@ app.get('/api/Procuradores/', (req, res)=>{
                 }
             });
         }
-    } );
+    });
+} );
+
+app.put('/api/persona/:id', (req, res)=>{
+
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "passa1234b",
+        database: "deswebq12023"
+    });
+
+    let sql = " update tbl_persona set nombre_persona = ?, " +
+                " apellido_persona = ?,  "+
+                " fecha_nacimiento = ? "+
+                " where id_persona = ? ";
+
+    let parametros = [  req.body.nombre_persona, 
+                        req.body.apellido_persona, 
+                        req.body.fecha_nacimiento, 
+                        req.params.id];
+
+    con.connect(function(err){
+
+        if (err){
+            res.send(err);
+        }else{
+            con.query(sql, parametros, function(err, result){
+                if (err){
+                    res.send(err);
+                }else{
+                    res.send(result);
+                }
+            });
+        }
+    });
 
 });
+
+app.delete('/api/persona/:id', (req, res)=>{
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "passa1234b",
+        database: "deswebq12023"
+    });
+
+    let sql = "delete from tbl_persona where id_persona = ?";
+
+    let parametros = [req.params.id];
+
+    con.connect(function(err){
+
+        if (err){
+            res.send(err);
+        }else{
+            con.query(sql, parametros, function(err, result){
+                if (err){
+                    res.send(err);
+                }else{
+                    res.send(result);
+                }
+            });
+        }
+    });
+
+});
+
+
+app.listen(7090);
